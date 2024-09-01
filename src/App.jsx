538d2a23e7fba50e7559194fc35cf1012254a35d
marriamdd <davitashvili.m3@gmail.com>
2024-09-01 20:44:44 +0400

    feat:implement router & layouting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MyDayPage from "./pages/MyDayPage";
+import ImportantPage from "./pages/ImportantsPage";
+import SingInPage from "./pages/SingInPage";
+import Layout from "./layouts/Layout";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SingInPage />} />
+          <Route path="/" element={<Layout />}>
+            <Route path="/MyDay" element={<MyDayPage />} />
+            <Route path="/Important" element={<ImportantPage />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/MyDayPage.jsx
@@ -0,0 +1,3 @@
+export default function MyDayPage() {
+  return <div>MyDayPage</div>;
+}
