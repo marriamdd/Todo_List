449ab1fd2771dbf6132d6aff9e535cf649414d96
marriamdd <davitashvili.m3@gmail.com>
2024-09-08 17:47:01 +0400

    f

--- src/pages/SingInPage.jsx
+++ src/pages/SingInPage.jsx
@@ -2,7 +2,7 @@ import { useUser } from "@clerk/clerk-react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { SignIn } from "@clerk/clerk-react";
-import LoginStaticCard from "/Users/mariamidavitashvili/Desktop/Todo_List/src/components/LoginStaticCard.jsx";
+
 export default function SignInPage() {
   const { user, isLoaded } = useUser();
   const navigate = useNavigate();
@@ -15,7 +15,6 @@ export default function SignInPage() {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <LoginStaticCard />
       <SignIn />
     </div>
   );
